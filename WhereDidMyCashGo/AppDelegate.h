9df245d35e3d8e751ff4aba51ccb43ce7036f82a
MelissaGarrett <melissa_garrett@hotmail.com>
2015-05-04 19:43:17 -0500

    Initial Commit

---
+++ WhereDidMyCashGo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhereDidMyCashGo
+//
+//  Created by Melissa  Garrett on 5/4/15.
+//  Copyright (c) 2015 MelissaGarrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhereDidMyCashGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhereDidMyCashGo
+//
+//  Created by Melissa  Garrett on 5/4/15.
+//  Copyright (c) 2015 MelissaGarrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
