8ae6982976382c47698381c132e8a6b019f29f69
MelissaGarrett <melissa_garrett@hotmail.com>
2015-08-02 18:37:39 -0500

    Intermediate checkin

---
+++ WhereDidMyCashGo/Transaction.h
@@ -0,0 +1,13 @@
+//
+//  Transaction.h
+//  WhereDidMyCashGo
+//
+//  Created by Melissa  Garrett on 5/11/15.
+//  Copyright (c) 2015 MelissaGarrett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Transaction : NSObject
+
+@end
---
+++ WhereDidMyCashGo/Transaction.m
@@ -0,0 +1,13 @@
+//
+//  Transaction.m
+//  WhereDidMyCashGo
+//
+//  Created by Melissa  Garrett on 5/11/15.
+//  Copyright (c) 2015 MelissaGarrett. All rights reserved.
+//
+
+#import "Transaction.h"
+
+@implementation Transaction
+
+@end
